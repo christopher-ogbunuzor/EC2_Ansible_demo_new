1d596d978d41f6f527b17f62f1f6a36b9884cc2c
Christopher <christopher.ogbunuzor@cloudreach.com>
2022-10-01 20:54:31 +0100

    Dockerise webserver and configure Jenkins EC2 server

--- ec2.tf
+++ ec2.tf
@@ -13,6 +13,14 @@ resource "aws_security_group" "my_app_sg" {
   }
 
   ingress {
+    description = "for jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["86.15.241.215/32"]
+  }
+
+  ingress {
     description = "SSH from my VPC"
     from_port   = 22
     to_port     = 22
@@ -50,19 +58,45 @@ resource "aws_security_group" "my_app_sg" {
 }
 
 data "aws_ami" "my_aws_ami" {
-    owners = ["099720109477"]
+    owners = ["099720109477"] 
     most_recent = true
     filter {
         name = "name"
-        values = [ "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*" ]
+        values = [ "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*" ]
     }
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "ec2_keypair" {
+  key_name   = var.keypair_name
+  public_key = tls_private_key.example.public_key_openssh
+
+  #  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
+  #   command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
+  # }
+}
+
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.ec2_keypair.key_name}.pem"
+  content = tls_private_key.example.private_key_pem
 }
 
 # EC2 - PUBLIC
 resource "aws_instance" "my_public_server" {
     ami = data.aws_ami.my_aws_ami.id
     instance_type = var.instance_type
-    key_name = var.keypair_name
+    key_name = aws_key_pair.ec2_keypair.key_name
     subnet_id = module.network.public_subnet_a_id
     vpc_security_group_ids = [ aws_security_group.my_app_sg.id ]
+
+    user_data = "${file("install_jenkins.sh")}"
+
+
+    tags = {
+    Name = "Jenkins Server"
+  }
 }
---
+++ install_jenkins.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install openjdk-11-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo ufw allow 8080
+
+sudo apt install docker.io -y
+sudo service docker start
+
+# # more
+sudo apt install curl
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+#         # sudo apt-get install python-pip -y
+        # sudo pip install docker-compose
+
